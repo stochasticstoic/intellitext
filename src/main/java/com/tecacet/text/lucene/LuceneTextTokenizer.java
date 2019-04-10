d1899bc42bac3a6fb269a8005ed6113ab18180fd
algorythmist <dimitri@tecacet.com>
2019-04-09 17:56:15 -0700

    removed unused

--- src/main/java/com/tecacet/text/lucene/LuceneTextTokenizer.java
+++ src/main/java/com/tecacet/text/lucene/LuceneTextTokenizer.java
@@ -1,63 +1,55 @@
 package com.tecacet.text.lucene;
 
+import com.tecacet.text.TextTokenizer;
+import com.tecacet.text.TokenizerCallback;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
-import org.apache.lucene.util.Version;
+public class LuceneTextTokenizer implements TextTokenizer, LuceneVersion {
+    private final Analyzer analyzer;
+    private TokenizerCallback callback = null;
 
-import com.tecacet.text.TextTokenizer;
-import com.tecacet.text.TokenizerCallback;
+    public LuceneTextTokenizer() {
+        this(new StandardAnalyzer(LUCENE_VERSION));
+    }
 
-public class LuceneTextTokenizer implements TextTokenizer,LuceneVersion
-{
-	private final Analyzer analyzer;
-	private TokenizerCallback callback = null;
+    public LuceneTextTokenizer(Analyzer analyzer) {
+        this.analyzer = analyzer;
+    }
 
-	public LuceneTextTokenizer() {
-		this(new StandardAnalyzer(LUCENE_VERSION));
-	}
-	
-	public LuceneTextTokenizer(Analyzer analyzer) {
-		this.analyzer = analyzer;
-	}
-
-	public LuceneTextTokenizer(Analyzer analyzer, TokenizerCallback callback) {
-		this.analyzer = analyzer;
-		this.callback = callback;
-	}
-	
-	public LuceneTextTokenizer(TokenizerCallback callback) {
+    public LuceneTextTokenizer(TokenizerCallback callback) {
         this();
         this.callback = callback;
     }
 
-	@Override
-	public List<String> tokenize(String text) throws IOException   {
-		List<String> tokens = new ArrayList<String>();
-		Reader reader = new StringReader(text);
-		TokenStream tokenStream = analyzer.tokenStream(null, reader);
+    @Override
+    public List<String> tokenize(String text) throws IOException {
+        List<String> tokens = new ArrayList<String>();
+        Reader reader = new StringReader(text);
+        TokenStream tokenStream = analyzer.tokenStream(null, reader);
 
-		try {
-            for (tokenStream.reset();tokenStream.incrementToken();) {
-            	CharTermAttribute token = tokenStream
-            			.getAttribute(CharTermAttribute.class);
-            	tokens.add(token.toString());
-            	if (callback != null) {
-            		callback.addToken(token.toString());
-            	}
+        try {
+            for (tokenStream.reset(); tokenStream.incrementToken(); ) {
+                CharTermAttribute token = tokenStream.getAttribute(CharTermAttribute.class);
+                tokens.add(token.toString());
+                if (callback != null) {
+                    callback.addToken(token.toString());
+                }
             }
             tokenStream.close();
         } catch (IOException e) {
             throw new RuntimeException(e); //TODO
         }
-		return tokens;
-	}
+        return tokens;
+    }
 
 }
--- src/main/java/com/tecacet/text/lucene/PorterStemStopWordAnalyzer.java
+++ src/main/java/com/tecacet/text/lucene/PorterStemStopWordAnalyzer.java
@@ -1,13 +1,5 @@
 package com.tecacet.text.lucene;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenFilter;
 import org.apache.lucene.analysis.Tokenizer;
@@ -19,60 +11,58 @@ import org.apache.lucene.analysis.synonym.SynonymFilter;
 import org.apache.lucene.analysis.synonym.SynonymMap;
 import org.apache.lucene.analysis.util.CharArraySet;
 import org.apache.lucene.util.CharsRef;
-import org.apache.lucene.util.Version;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public final class PorterStemStopWordAnalyzer extends Analyzer implements LuceneVersion {
 
-	private final Set<String> stopwords = new HashSet<String>();
+    private final Set<String> stopwords = new HashSet<String>();
 
-	// TODO generalize to many-to-one
-	private Map<String, String> synonyms = new HashMap<String, String>();
+    // TODO generalize to many-to-one
+    private Map<String, String> synonyms = new HashMap<String, String>();
 
-	public PorterStemStopWordAnalyzer() {
-		super();
-	}
+    public PorterStemStopWordAnalyzer(Collection<String> stopWords) throws IOException {
+        super();
+        stopwords.addAll(stopWords);
+    }
 
-	public PorterStemStopWordAnalyzer(Collection<String> stopWords)
-			throws IOException {
-		super();
-		stopwords.addAll(stopWords);
-	}
+    private TokenFilter addSynomyms(TokenFilter stream) {
+        if (synonyms.isEmpty()) {
+            return stream;
+        }
+        SynonymMap.Builder builder = new SynonymMap.Builder(true);
+        for (Map.Entry<String, String> entry : synonyms.entrySet()) {
+            builder.add(new CharsRef(entry.getKey()), new CharsRef(entry.getValue()), true);
+        }
+        SynonymMap map;
+        try {
+            map = builder.build();
+            return new SynonymFilter(stream, map, true);
+        } catch (IOException e) {
+            throw new RuntimeException(e); // TODO
+        }
 
-	private TokenFilter addSynomyms(TokenFilter stream) {
-		if (synonyms.isEmpty()) {
-			return stream;
-		}
-		SynonymMap.Builder builder = new SynonymMap.Builder(true);
-		for (Map.Entry<String, String> entry : synonyms.entrySet()) {
-			builder.add(new CharsRef(entry.getKey()),
-					new CharsRef(entry.getValue()), true);
-		}
-		SynonymMap map;
-		try {
-			map = builder.build();
-			return new SynonymFilter(stream, map, true);
-		} catch (IOException e) {
-			throw new RuntimeException(e); // TODO
-		}
+    }
 
-	}
+    public void addSynonym(String word1, String word2) {
+        synonyms.put(word1, word2);
+    }
 
-	public void addSynonym(String word1, String word2) {
-		synonyms.put(word1, word2);
-	}
-
-	@Override
-	protected TokenStreamComponents createComponents(String fieldName,
-			Reader reader) {
-		Tokenizer tokenizer = new StandardTokenizer(LUCENE_VERSION, reader);
-		TokenFilter lowerCaseFilter = new LowerCaseFilter(LUCENE_VERSION,
-				tokenizer);
-		TokenFilter synonymFilter = addSynomyms(lowerCaseFilter);
-		CharArraySet set = new CharArraySet(LUCENE_VERSION, stopwords, true);
-		TokenFilter stopFilter = new StopFilter(LUCENE_VERSION, synonymFilter,
-				set);
-		TokenFilter stemFilter = new PorterStemFilter(stopFilter);
-		return new TokenStreamComponents(tokenizer, stemFilter);
-	}
+    @Override
+    protected TokenStreamComponents createComponents(String fieldName, Reader reader) {
+        Tokenizer tokenizer = new StandardTokenizer(LUCENE_VERSION, reader);
+        TokenFilter lowerCaseFilter = new LowerCaseFilter(LUCENE_VERSION, tokenizer);
+        TokenFilter synonymFilter = addSynomyms(lowerCaseFilter);
+        CharArraySet set = new CharArraySet(LUCENE_VERSION, stopwords, true);
+        TokenFilter stopFilter = new StopFilter(LUCENE_VERSION, synonymFilter, set);
+        TokenFilter stemFilter = new PorterStemFilter(stopFilter);
+        return new TokenStreamComponents(tokenizer, stemFilter);
+    }
 
 }
